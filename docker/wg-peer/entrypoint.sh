ba6749809557c7d35471a98ae4ce00f064c730f3
Vitalii <step@topli.ch>
2025-06-09 23:18:53 +0200

    Create entrypoint.sh

---
+++ docker/wg-peer/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+mkdir -p /dev/net
+[ -e /dev/net/tun ] || mknod /dev/net/tun c 10 200
+chmod 600 /dev/net/tun
+
+export WG_I_PREFER_BUGGY_USERSPACE_TO_POLISHED_KMOD=1
+wg-quick down wg0 || true
+wg-quick up wg0 || { echo "!!! Не вдалося підняти wg0"; exit 1; }
+
+tail -f /dev/null
