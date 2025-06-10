8e63cef040cda7af0e4c4c7238c489b5e3b5cebb
Vitalii <step@topli.ch>
2025-06-10 12:28:21 +0200

    Update entrypoint.sh

--- docker/wg-peer/entrypoint.sh
+++ docker/wg-peer/entrypoint.sh
@@ -7,6 +7,6 @@ chmod 600 /dev/net/tun
 
 export WG_I_PREFER_BUGGY_USERSPACE_TO_POLISHED_KMOD=1
 wg-quick down wg0 || true
-wg-quick up wg0 || { echo "!!! Не вдалося підняти wg0"; exit 1; }
+wg-quick up wg0 || { echo "!!! Failed to bring up tunnel"; exit 1; }
 
 tail -f /dev/null
