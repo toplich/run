dbae668b21ace90257ecb6accafad6c0b76f3474
Vitalii <step@topli.ch>
2025-06-11 22:36:51 +0200

    Create task5min.ps1

---
+++ ps/task5min.ps1
@@ -0,0 +1,16 @@
+$TaskName = "iSCSI Monitor"
+$ScriptPath = "C:\Scripts\check-iscsi.ps1"
+
+# Дія: запуск PowerShell із потрібним скриптом
+$Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$ScriptPath`""
+
+# Тригер: запуск один раз сьогодні і повторення кожні 5 хвилин протягом 24 годин
+$Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date `
+    -RepetitionInterval (New-TimeSpan -Minutes 5) `
+    -RepetitionDuration (New-TimeSpan -Days 1)
+
+# Права адміністратора
+$Principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -RunLevel Highest
+
+# Реєстрація завдання
+Register-ScheduledTask -TaskName $TaskName -Trigger $Trigger -Action $Action -Principal $Principal -Force
