b0b09913e5064e02e93ba13f3f40e1bf39b24d6d
Vitalii <step@topli.ch>
2025-11-27 08:45:21 +0100

    Update Get-DomainWindowsVersions.ps1

--- ps/Get-DomainWindowsVersions.ps1
+++ ps/Get-DomainWindowsVersions.ps1
@@ -1,17 +1,29 @@
 <# 
 .SYNOPSIS
-    Quick inventory: OS Name + Version + Build from AD
+    Quick inventory: OS Name + Version + Build + IP via DNS lookup
 #>
 
 Import-Module ActiveDirectory
 
-Get-ADComputer -Filter 'Enabled -eq $True' -Properties OperatingSystem, OperatingSystemVersion, lastLogonTimestamp |
+Get-ADComputer -Filter 'Enabled -eq $True' -Properties OperatingSystem, OperatingSystemVersion, lastLogonTimestamp, dNSHostName |
     Select-Object `
         Name,
         OperatingSystem,
         OperatingSystemVersion,
-        @{ n='LastLogon'; e={[DateTime]::FromFileTime($_.lastLogonTimestamp)} } |
+        @{ n='LastLogon'; e={ [DateTime]::FromFileTime($_.lastLogonTimestamp) } },
+        @{ n='IPAddress';  e={
+                try {
+                    # Very fast DNS lookup
+                    ([System.Net.Dns]::GetHostAddresses($_.dNSHostName) | 
+                     Where-Object { $_.AddressFamily -eq "InterNetwork" } |
+                     Select-Object -First 1).IPAddressToString
+                }
+                catch {
+                    "Unknown"
+                }
+            }
+        } |
     Sort-Object Name |
-    Export-Csv "C:\inetpub\AD_OS_Inventory.csv" -NoTypeInformation
+    Export-Csv "C:\Reports\AD_OS_Inventory.csv" -NoTypeInformation
 
-Write-Host "Report saved to C:\inetpub\AD_OS_Inventory.csv"
+Write-Host "Report saved to C:\Reports\AD_OS_Inventory.csv"
